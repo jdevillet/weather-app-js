407dd3e8b16e42e96f18bb392f0728b81742b5cc
Jdevillet <jordandevilletpro@gmail.com>
2025-10-15 15:02:34 +0200

    feat: HTML structure

---
+++ index.js
@@ -0,0 +1,6 @@
+// TODO
+// - Recherche météo par ville
+// - Affichage température, humidité, vent
+// - Changement automatique d'icône selon météo
+// - Historique des recherches (localStorage)
+// - Dark mode avec sauvegarde
