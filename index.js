dc90aa6f0f7ee492598f8284f3d5da4a0f11852b
Jdevillet <jordandevilletpro@gmail.com>
2025-10-15 17:10:20 +0200

    feat: install axios and declare JS constants

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-// TODO
+// todo
 // - Recherche météo par ville
 // - Affichage température, humidité, vent
 // - Changement automatique d'icône selon météo
 // - Historique des recherches (localStorage)
 // - Dark mode avec sauvegarde
+
+const searchBar = document.getElementById("searchBar");
+const btnSubmit = document.getElementById("submitBtn");
+const locationCity = document.querySelector(".location");
+const todayDate = document.querySelector(".today-date");
+const todayWeather = document.querySelector(".today-weather");
+const weatherIllustration = document.querySelector(".weather-img");
+const todayTemp = document.querySelector(".today-temperature");
+const windSpeed = document.querySelector(".wind-speed");
+const rainChance = document.querySelector(".rain-chance");
