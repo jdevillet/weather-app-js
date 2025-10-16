1dbb4a9d740f2f29450d7a133c5f8b171de36e18
Jdevillet <jordandevilletpro@gmail.com>
2025-10-16 17:14:44 +0200

    feat: add city search, default city and updateUI function
    - Implement city weather search with form submission
    - Set default city to London for initial display
    - Create updateUI to dynamically show weather data and icons

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// todo
-// - Recherche météo par ville
-// - Affichage température, humidité, vent
-// - Changement automatique d'icône selon météo
-// - Historique des recherches (localStorage)
-// - Dark mode avec sauvegarde
-
+const userForm = document.querySelector(".request-form");
 const searchBar = document.getElementById("searchBar");
 const btnSubmit = document.getElementById("submitBtn");
 const locationCity = document.querySelector(".location");
@@ -15,26 +9,101 @@ const todayTemp = document.querySelector(".today-temperature");
 const windSpeed = document.querySelector(".wind-speed");
 const rainChance = document.querySelector(".rain-chance");
 
-// let weatherData = {};
-
-// async function fetchWeather() {
-//   try {
-//     const res = await axios.get(
-//       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key={insertYourAPIkey}"
-//     );
-//     weatherData = res.data;
-//     console.log(weatherData);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// fetchWeather();
+const defaultCity = "London";
 let weatherData = {};
 
-fetch("mockWeatherData.json")
-  .then((res) => res.json())
-  .then((data) => {
-    weatherData = data;
+fetchWeather(defaultCity);
+
+userForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const cityRequest = searchBar.value.trim();
+
+  if (!cityRequest) {
+    console.log("Enter a city please");
+    return;
+  }
+  await fetchWeather(cityRequest);
+});
+
+async function fetchWeather(city) {
+  try {
+    const res = await axios.get(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key={PUT YOUR API KEY HERE}&unitGroup=metric`
+    );
+    weatherData = res.data;
     console.log(weatherData);
-  });
+
+    updateUI();
+  } catch (err) {
+    console.error("Couldn't load weather data", err);
+  }
+}
+
+// ==========================
+// card info display
+// ==================
+const updateUI = () => {
+  // date formater
+  let dateToFormat = weatherData.days[0].datetime;
+  const dateObj = new Date(dateToFormat);
+  // --
+
+  locationCity.textContent = weatherData.resolvedAddress;
+  todayDate.textContent = dateObj.toLocaleDateString("fr-FR");
+
+  todayWeather.textContent = weatherData.currentConditions.conditions;
+  todayTemp.textContent = `${weatherData.currentConditions.temp}°C`;
+  windSpeed.textContent = `${weatherData.currentConditions.windspeed}kp/h`;
+  rainChance.textContent = `${weatherData.currentConditions.precipprob}%`;
+
+  updateWeatherImage(weatherData.currentConditions.icon);
+};
+
+function updateWeatherImage(icon) {
+  let svgUrl;
+
+  switch (icon) {
+    case "clear-day":
+      svgUrl = "./img/sun.svg";
+      break;
+    case "clear-night":
+      svgUrl = "./img/clear-night.svg";
+      break;
+    case "partly-cloudy-day":
+      svgUrl = "./img/cloudy.svg";
+      break;
+    case "cloudy":
+      svgUrl = "./img/cloudy.svg";
+      break;
+    case "partly-cloudy-night":
+      svgUrl = "./img/partly-cloudy-night.svg";
+      break;
+    case "rain":
+      svgUrl = "./img/rain.svg";
+      break;
+    case "snow":
+      svgUrl = "./img/snowflake.svg";
+      break;
+    case "thunderstorm":
+      svgUrl = "./img/thunderstorm.svg";
+      break;
+    case "fog":
+      svgUrl = "./img/fog.svg";
+      break;
+    case "wind":
+      svgUrl = "./img/wind.svg";
+      break;
+    default:
+      svgUrl = "./img/default.svg";
+  }
+
+  fetch(svgUrl)
+    .then((response) => response.text())
+    .then((svgContent) => {
+      weatherIllustration.innerHTML = svgContent;
+      const svg = weatherIllustration.querySelector("svg");
+    })
+    .catch(() => {
+      weatherIllustration.innerHTML = "";
+    });
+}
